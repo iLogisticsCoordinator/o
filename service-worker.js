ef08a535ebdac3f62559f5af4e410311fa887fb8
ΛИ❍ИƔM❍US <129817332+iLogisticsCoordinator@users.noreply.github.com>
2025-04-14 19:21:38 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,14 @@
-self.addEventListener('install', (e) => {
-  console.log('NebulaDash Service Worker instalado');
+self.addEventListener('install', e => {
+  console.log('Service Worker instalado');
   e.waitUntil(self.skipWaiting());
 });
 
-self.addEventListener('activate', (e) => {
-  console.log('NebulaDash Service Worker activado');
+self.addEventListener('activate', e => {
+  console.log('Service Worker activado');
   e.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', e => {
   e.respondWith(fetch(e.request));
 });
+
